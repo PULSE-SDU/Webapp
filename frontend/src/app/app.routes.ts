74a774b5c6263cab5c8b7e8f692cfec55f2a62e8
Rita <139698747+pastelnata@users.noreply.github.com>
2025-11-12 13:14:31 +0100

    Feat/app base (#14)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // TODO: add real routes here (remove redirect to) and do:
+  // example:  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', redirectTo: '' },
+  { path: 'tag-list', redirectTo: '' },
+  { path: 'analytics', redirectTo: '' },
+  { path: '**', redirectTo: '' },
+];
