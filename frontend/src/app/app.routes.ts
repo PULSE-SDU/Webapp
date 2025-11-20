4c13937b86b93d851635e6428f9bfce8ea612084
Altan Esmer <esmeraltan@gmail.com>
2025-11-20 11:23:13 +0100

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,7 +5,8 @@ export const routes: Routes = [
   // TODO: add real routes here (remove redirect to) and do:
   // example:  { path: 'dashboard', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'tag-list', redirectTo: '' },
-  { path: 'analytics', redirectTo: '' },
+  { path: 'tag-list', redirectTo: 'dashboard' },
+  { path: 'analytics', redirectTo: 'dashboard' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'dashboard' },
 ];
