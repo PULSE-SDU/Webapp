4e7bfa8a490b5cacde9ed5f33e7f53371bb75885
kramoda <pavliukiana5@gmail.com>
2025-12-03 18:22:55 +0100

    feat:basic analytics page for demonstration purposes

--- frontend/src/app/pages/analytics/analytics/analytics.ts
+++ frontend/src/app/pages/analytics/analytics/analytics.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StackedAreaChart } from '../../../shared/components/stacked-area-chart/stacked-area-chart';
-import { InfoCards } from '../../../shared/components/info-cards/info-cards';
+import { BatteryHistoryGraphComponent } from '../../../shared/components/battery-history-graph/battery-history-graph.component';
+import { StatusDistributionChartComponent } from '../../../shared/components/status-distribution-chart/status-distribution-chart.component';
 
 @Component({
   selector: 'app-analytics',
-  imports: [CommonModule, StackedAreaChart, InfoCards],
+  imports: [CommonModule, StackedAreaChart, BatteryHistoryGraphComponent, StatusDistributionChartComponent],
   templateUrl: './analytics.html',
   styleUrl: './analytics.scss',
 })
