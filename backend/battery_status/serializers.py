23c21abde4979454166074bf3f7fea4b7f09502d
kramoda <pavliukiana5@gmail.com>
2025-12-07 15:58:58 +0100

    feat: add StatusSummary API views for historical and latest status summaries

--- backend/battery_status/serializers.py
+++ backend/battery_status/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import BatteryStatus
+from .models import BatteryStatus, StatusSummary
 
 
 class BatteryStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = BatteryStatus
+        fields = "__all__"
+
+class StatusSummarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StatusSummary
         fields = "__all__"
